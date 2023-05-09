cfc40c12ee021a59c62dea8f6bf9070fea73d465
Rae62 <ludovic.duhamel-62260@outlook.fr>
2023-05-09 11:59:15 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { getAuth, FacebookAuthProvider, signInWithCredential} from 'firebase/auth';
+import { firebase } from './config';
+import { LoginManager, AccessToken} from 'react-native-fbsdk-next';
+import React, {useState , useEffect} from 'react';
+import 'expo-dev-client';
 export default function App() {
-  return (
+  const [initializing , setInitializing] = useState(true);
+  const [user , setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  const signInWithFB = async () => {
+    try {
+      await LoginManager.logInWithPermissions(['public-profile', 'email']);
+      const data = await AccessToken.getCurrentAccessToken();
+      if(!data) {
+        return;
+      }
+      const facebookCredential = FacebookAuthProvider.credential(data.accessToken);
+      const auth = getAuth();
+      const response = 
+      await signInWithCredential(firebase.auth(), facebookCredential);
+      console.log(response);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+ const signOut = async () => {
+  try{
+    await firebase.auth().signOut();
+  } catch(e) {
+    console.log(e);
+  }
+ }
+
+  if(initializing) return null ; 
+  
+  if(!user){
+    return (
+      <View style={styles.container}>
+        <Text>Sign in with Facebook </Text>
+        <Button title='Sign in with Facebook' onPress={signInWithFB}></Button>
+      </View>
+    );
+  }
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Sign in fb</Text>
+      <View style={{marginTop:100, alignItems:'center'}}>
+        <Text>Welcome ! {user.displayName}</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPg8dYreTUnik8XObkXx9II1o61Y6u7eE",
+    authDomain: "authsocial-55b11.firebaseapp.com",
+    projectId: "authsocial-55b11",
+    storageBucket: "authsocial-55b11.appspot.com",
+    messagingSenderId: "110184963452",
+    appId: "1:110184963452:web:2baff37934c6b37f6558f9"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  export { firebase };
